c3809a5b68b972e25f6d2cda6a57fca2a00b88e1
Brandy Knust <brandy_1175@hotmail.com>
2021-02-28 19:02:41 -0600

    commit 2 2-28

--- WeatherPy/api_keys/api_keys.py
+++ WeatherPy/api_keys/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "a845b5256dedcb0b8ccb7253f7b5b324"
+weather_api_key = "876deee2970de6d89b1d4ac721f15543"
 
 # Google API Key
 g_key = "AIzaSyD2_hMWQwWF0Eq7ANPOlhKzR3KldNPp5ho"
